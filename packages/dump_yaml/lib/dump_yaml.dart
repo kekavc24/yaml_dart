77a63713c4c07492ddd7f6e7af48165f2f652046
Kelvin Kavisi <68240897+kekavc24@users.noreply.github.com>
2026-06-27 22:15:57 +0300

    chore(dumper): remove stale `GTag` export
    - Export `InjectState` instead.

--- packages/dump_yaml/lib/dump_yaml.dart
+++ packages/dump_yaml/lib/dump_yaml.dart
@@ -6,7 +6,8 @@ export 'src/dumper/dumper.dart' show YamlBuffer;
 export 'src/dumper/inline_flow_dumper.dart';
 export 'src/dumper/yaml_dumper.dart';
 export 'src/event_tree/node.dart' hide Doc, isRecursiveAnchorRef;
-export 'src/event_tree/tree_builder.dart' show GTags, PathLogger, TreeBuilder;
+export 'src/event_tree/tree_builder.dart'
+    show InjectState, PathLogger, TreeBuilder;
 export 'src/strings.dart' show Normalized, splitUnfoldScanned;
 export 'src/unfolding.dart';
 export 'src/views/dumpable.dart';
